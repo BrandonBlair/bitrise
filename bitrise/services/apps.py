f432c9d96b86d96ee5df4e8d9d483f17ec38263a
Brandon Blair <brandon.blair@rewardstyle.com>
2018-06-04 16:42:57 -0500

    Add bitrise app object

--- bitrise/services/apps.py
+++ bitrise/services/apps.py
@@ -2,10 +2,13 @@ from slimpoint.service import Endpoint
 
 
 class BitriseApp(object):
-    def __init__(self, **kwargs):
+    def __init__(self, app_data):
         """Represents a Bitrise App as depicted via JSON from Bitrise
 
         Args:
+            app_data (dict): App data as received from Bitrise
+
+        App data contents:
             slug (str): Unique identifier for app
             title (str): App title
             project_type (str): ios, etc
@@ -16,24 +19,20 @@ class BitriseApp(object):
             is_disabled (bool): Is the app currently disabled?
             status (int): Status of app in Bitrise
             is_public (bool): Whether or not app is public
-            owner_account_type (str): organization, etc
-            owner_name (str): Name of app owner
-            owner_slug (str): Unique ID of owner
+            owner (dict):
+                account_type (str): organization, etc
+                name (str): Name of app owner
+                slug (str): Unique ID of owner
         """
 
-        self.slug = kwargs.get('slug')
-        self.title = kwargs.get('title')
-        self.project_type = kwargs.get('project_type')
-        self.provider = kwargs.get('provider')
-        self.repo_owner = kwargs.get('repo_owner')
-        self.repo_url = kwargs.get('repo_url')
-        self.repo_slug = kwargs.get('repo_slug')
-        self.is_disabled = kwargs.get('is_disabled')
-        self.status = kwargs.get('status')
-        self.is_public = kwargs.get('is_public')
-        self.owner_account_type = kwargs.get('owner_account_type')
-        self.owner_name = kwargs.get('owner_name')
-        self.owner_slug = kwargs.get('owner_slug')
+        self.data = app_data
+
+        for attr in app_data:
+            setattr(self, attr, app_data[attr])
+
+    @property
+    def json(self):
+        return self.data
 
 
 class AppsEndpoint(Endpoint):
