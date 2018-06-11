d628b0070f4d98dc34bc4cfa9ce80078c0ef1323
Brandon Blair <brandon.blair@rewardstyle.com>
2018-06-11 12:47:50 -0500

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitrise',
-    version='1.0.0',
+    version='1.0.1',
     description='Python bindings for Bitrise REST API',
     author='Brandon Blair',
     author_email='cbrandon.blair@gmail.com',
